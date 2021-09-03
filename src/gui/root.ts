eecd849ca3bb28ae2f8c65892a2c113bb1ed3616
McKittrick Kaminski <mckittrick.kaminski@gmail.com>
2021-09-02 21:34:07 -0500

    basic first draft of lempira coin

--- src/gui/root.ts
+++ src/gui/root.ts
@@ -3,17 +3,21 @@ import {property, customElement, state} from 'lit/decorators.js';
 import {ScopedElementsMixin as scope} from '@open-wc/scoped-elements';
 import NotConnected from './Views/notConnected';
 import {colors, fonts} from './styles';
-import Dashboard from './Views/Dashboard';
 import {connect} from 'pwa-helpers';
 import {RootState, store} from '#services/redux/store';
-import {faUsers, faHome} from '@fortawesome/free-solid-svg-icons';
+import {faUsers, faHome, faMoneyBillWave} from '@fortawesome/free-solid-svg-icons';
 import FaIcon from '#components/faIcon';
+import ManagerView from './Views/Manager/ManagerView';
+import OwnerView from './Views/Owner/OwnerView';
+import CustomerView from './Views/Customer/customerView';
 
 @customElement('root-el')
 export default class RootElement extends connect(store)(scope(LitElement)) {
 	@property({attribute: false}) connected = false;
 	@state() private isOwner = false;
 	@state() private isManager = false;
+
+	@state() private view: 'home' | 'manager' | 'owner' = 'home';
 
 	stateChanged(state: RootState) {
 		this.connected = state.eth.connected;
@@ -26,15 +30,28 @@ export default class RootElement extends connect(store)(scope(LitElement)) {
 	}
 
 	goOwner() {
-		console.log('owner view');
+		this.view = 'owner';
 	}
 
 	goManager() {
-		console.log('manager view');
+		this.view = 'manager';
 	}
 
 	goHome() {
-		console.log('home view');
+		this.view = 'home';
+	}
+
+	renderView() {
+		switch (this.view) {
+			case 'home':
+				return html`<customer-view></customer-view>`;
+			case 'manager':
+				return html`<manager-view></manager-view>`;
+			case 'owner':
+				return html`<owner-view></owner-view>`;
+			default:
+				return html``;
+		}
 	}
 
 	render() {
@@ -47,11 +64,11 @@ export default class RootElement extends connect(store)(scope(LitElement)) {
 						<lc-icon class="icon" .icon=${faUsers} action @clicked=${this.goOwner} circle></lc-icon>
 					` : ''}
 					${this.isManager ? html`
-						<lc-icon></lc-icon>
+						<lc-icon class="icon" .icon=${faMoneyBillWave} action @clicked=${this.goManager}></lc-icon>
 					` : ''}
 				</div>
 				` : ''}
-				${this.connected ? html`<dashboard-el></dashboard-el>` : html`<not-connected></not-connected>`}
+				${this.renderView()}
 			</div>
 		`;
 	}
@@ -62,6 +79,9 @@ export default class RootElement extends connect(store)(scope(LitElement)) {
 			width: 100vw;
 			background: var(--bg);
 			position: relative;
+			display: flex;
+			align-items: center;
+			justify-content: center;
 		}
 		.icon {
 			width: 3rem;
@@ -73,6 +93,7 @@ export default class RootElement extends connect(store)(scope(LitElement)) {
 		.nav {
 			display: flex;
 			position: absolute;
+			top: 0;
 			left: 0;
 			right: 0;
 			width: fit-content;
@@ -86,7 +107,9 @@ export default class RootElement extends connect(store)(scope(LitElement)) {
 	static get scopedElements() {
 		return {
 			'not-connected': NotConnected,
-			'dashboard-el': Dashboard,
+			'manager-view': ManagerView,
+			'owner-view': OwnerView,
+			'customer-view': CustomerView,
 			'lc-icon': FaIcon,
 		};
 	}
